6ef52f0a94ff413445290e3a55fa09b5207894c9
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2025-08-17 12:01:11 +0300

    Feat(class): add constructor to the two classes

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -4,4 +4,43 @@ public class Femme {
     private String nom;
     private  String prenom;
     private int age;
+    
+    // Constructeur par défaut
+    public Femme() {
+        this.nom = "";
+        this.prenom = "";
+        this.age = 0;
+    }
+    
+    // Constructeur avec paramètres
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    // Getters et setters
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public String getPrenom() {
+        return prenom;
+    }
+    
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
