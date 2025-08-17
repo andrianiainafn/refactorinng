f23b5989b11c936ad76e02431daf2d8b03256345
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2025-08-17 11:40:08 +0300

    Feat(class): create Homme annd Femme class

---
+++ src/iut/bad/Femme.java
@@ -0,0 +1,7 @@
+package iut.bad;
+
+public class Femme {
+    private String nom;
+    private  String prenom;
+    private int age;
+}
---
+++ src/iut/bad/Homme.java
@@ -0,0 +1,7 @@
+package iut.bad;
+
+public class Homme {
+    private String nom;
+    private String prenom;
+    private int age;
+}
