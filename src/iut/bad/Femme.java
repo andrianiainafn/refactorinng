32eb3feac4e7896500736b69490dcddf00f5f570
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2025-08-17 12:45:10 +0300

    feat(class): move attribut to human class

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -1,48 +1,14 @@
 package iut.bad;
 
-import iut.Humain;
-
 public class Femme extends Humain {
-    private String nom;
-    private  String prenom;
-    private int age;
     
     // Constructeur par défaut
     public Femme() {
-        this.nom = "";
-        this.prenom = "";
-        this.age = 0;
+        super();
     }
     
     // Constructeur avec paramètres
     public Femme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
-    }
-    
-    // Getters et setters
-    public String getNom() {
-        return nom;
-    }
-    
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-    
-    public String getPrenom() {
-        return prenom;
-    }
-    
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-    
-    public int getAge() {
-        return age;
-    }
-    
-    public void setAge(int age) {
-        this.age = age;
+        super(nom, prenom, age);
     }
 }
---
+++ src/iut/bad/Humain.java
@@ -0,0 +1,48 @@
+package iut.bad;
+
+public class Humain {
+    // Classe parente pour Homme et Femme
+    // Les attributs sont maintenant dans la classe parente
+    protected String nom;
+    protected String prenom;
+    protected int age;
+    
+    // Constructeur par défaut
+    public Humain() {
+        this.nom = "";
+        this.prenom = "";
+        this.age = 0;
+    }
+    
+    // Constructeur avec paramètres
+    public Humain(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    // Getters et setters
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public String getPrenom() {
+        return prenom;
+    }
+    
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
